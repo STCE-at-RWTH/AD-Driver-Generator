a99110055cb310e05b786346aec21534e59c8a11
Felix Schurk <felix.schurk@rwth-aachen.de>
2023-10-30 16:03:05 +0100

    update CMakeLists to use option to enable and disable unit tests

--- src/try.test.cpp
+++ src/try.test.cpp
@@ -1,13 +1,12 @@
 #include "gtest/gtest.h"
-#include "try.cpp"
 
-TEST(DualNumber, Addition) {
-  DualNumber<double> x(1, 1);
-  DualNumber<double> y(2, 2);
-  DualNumber<double> z = x + y;
-  EXPECT_EQ(z.value, 3);
-  EXPECT_EQ(z.tangent, 3);
-}
+// TEST(DualNumber, Addition) {
+//   DualNumber<double> x(1, 1);
+//   DualNumber<double> y(2, 2);
+//   DualNumber<double> z = x + y;
+//   EXPECT_EQ(z.value, 3);
+//   EXPECT_EQ(z.tangent, 3);
+// }
 
 TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
