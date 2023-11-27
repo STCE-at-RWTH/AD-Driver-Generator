11bdcf5b148ec8845de74bcd1891653d14c0a39d
Felix Schurk <felix.schurk@rwth-aachen.de>
2023-11-27 16:26:12 +0100

    change to utilities for testing and add them to the cmake project

--- src/utilities.test.cpp
+++ src/utilities.test.cpp
@@ -5,7 +5,7 @@ TEST(SetSeedValue, BasicInput) {
     std::string variable = "x";
     std::string type_of_variable = "double";
     std::string value_for_seeding = "1.0";
-    std::string expected = "DualNumber<double> x(1.0, 1.0);";
+    std::string expected = "double x = 1.0";
     std::string actual = setSeedValue(variable, type_of_variable, value_for_seeding);
     EXPECT_EQ(actual, expected);
 }
