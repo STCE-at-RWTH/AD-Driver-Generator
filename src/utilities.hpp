c3e67415b94603f5646995f04623d4241f569335
Felix Schurk <felix.schurk@rwth-aachen.de>
2023-11-29 12:51:00 +0100

    add stringSplit function as well as test for them

--- src/utilities.hpp
+++ src/utilities.hpp
@@ -1,6 +1,13 @@
+#include <vector>
+#include <string>
+
 #ifndef SISC_LAB_UTILITIES_HPP
 #define SISC_LAB_UTILITIES_HPP
 
 std::string setSeedValue(std::string variable, std::string type_of_variable, std::string value_for_seeding);
+
+std::string getTypeOfVariable(std::string callSignature, std::string variableName);
+
+std::vector<std::string> splitString(std::string inputString, char delimiter);
 
 #endif //SISC_LAB_UTILITIES_HPP
--- src/utilities.test.cpp
+++ src/utilities.test.cpp
@@ -1,11 +1,85 @@
 #include "gtest/gtest.h"
 #include "utilities.hpp"
 
-TEST(SetSeedValue, BasicInput) {
+TEST(SetSeedValue, BasicInput)
+{
     std::string variable = "x";
     std::string type_of_variable = "double";
     std::string value_for_seeding = "1.0";
     std::string expected = "double x = 1.0";
     std::string actual = setSeedValue(variable, type_of_variable, value_for_seeding);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(SplitString, WhiteSpace_as_delimiter)
+{
+    // SETUP
+    std::string inputString{"abc def"};
+    char delimiter{' '};
+    auto some_test = " ";
+    std::vector<std::string> expected{"abc", "def"};
+
+    // ACT
+    auto actual = splitString(inputString, delimiter);
+
+    // ASSERT
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(SplitString, Split_at_commata)
+{
+    // SETUP
+    std::string inputString{"abc,def"};
+    char delimiter{','};
+    std::vector<std::string> expected{"abc", "def"};
+
+    // ACT
+    auto actual = splitString(inputString, delimiter);
+
+    // ASSERT
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(SplitString, Delimiter_not_in_string)
+{
+    // SETUP
+    std::string inputString{"abcdef"};
+    char delimiter{' '};
+    std::vector<std::string> expected{"abcdef"};
+
+    // ACT
+    auto actual = splitString(inputString, delimiter);
+
+    // ASSERT
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(SplitString, WhiteSpace_and_Brackets)
+{
+    // SETUP
+    std::string inputString{"(abc def"};
+    std::string delimiter{'(', ' '};
+
+    std::vector<std::string> expected{"abc", "def"};
+    std::vector<std::string> actual;
+    // ACT
+    for (auto delim : delimiter)
+        actual = splitString(inputString, delim);
+
+    // ASSERT
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(GetTypeOfVariable, SimpleInput_with_space_delimiter)
+{
+    // SETUP
+    std::string signature{"double x"};
+    std::string active_variable{"x"};
+    std::string expected{"double"};
+
+    // ACT
+    auto actual = getTypeOfVariable(signature, active_variable);
+
+    // ASSERT
     EXPECT_EQ(actual, expected);
 }
